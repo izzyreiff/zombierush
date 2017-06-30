8df5b2673060bdf63c57c62c6bbc2e04138facd3
Izzy Reiff <izzyreiff@gmail.com>
2017-06-30 17:19:41 +0000

    Added functional score counter

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 window.onload = function() {
     var titleEl = document.querySelector('.title-screen');
     var endEl = document.querySelector('.end-screen');
-    var enemies = document.querySelectorAll('[class="clickable"]');
+    var enemies = document.querySelectorAll('[id^="z"]');
     var deadEnemies = [];
     var scoreEl = document.querySelector('.score');
     var score = 0;
     var increaseCounter = function(e) {
         var enemy = e.currentTarget;
         if (deadEnemies.indexOf(enemy) != -1) {
-            return; }
+            return; 
+        }
         deadEnemies.push(enemy);
-        score+=1;
+        score+=100;
         scoreEl.innerHTML = 'Score: ' + score;
         if (enemies.length === deadEnemies.length) {
             endEl.style.display = 'block';
         }
     }
-    
-    var deadZombies = [];
-    var zombies = document.query
-    
-    
-    
     document.getElementById('startDaGame').addEventListener('click', function(){
         document.getElementById('startDaGame').setAttribute('style', 'display: none; visibility: hidden;')
     })
     enemies = Array.prototype.slice.call(enemies);
     enemies.forEach(function(enemyEl) {
-        enemyEl.addEventListener('hit', increaseCounter);
+        enemyEl.addEventListener('click', increaseCounter);
     });
 };
